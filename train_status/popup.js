f2a0201d13ac92f6141297b4c05ee7ae275e5479
Nayan Shah <nayanmange@gmail.com>
2012-03-31 22:52:07 +0530

    First commit.

---
+++ train_status/popup.js
@@ -0,0 +1,54 @@
+/**
+ * Performs an XMLHttpRequest to Train Enquiry to get running status
+ *
+ * @param callback Function If the response from fetching url has a
+ *     HTTP status of 200, this function is called with a JSON decoded
+ *     response.  Otherwise, this function is called with null.
+ * @param params String The url encoded parameters
+ */
+function fetchData(callback, params) {
+    var url = 'http://www.trainenquiry.com/RunningIslTrSt.aspx?' + params;
+    url = 'http://localhost/work/train/trainenquiry.html?' + params; // For development only
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(data) {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                callback(xhr.responseText);
+            } else {
+                callback(null);
+            }
+        }
+    }
+    $('#result').html(url);
+    //xhr.open('GET', url, true);
+    //xhr.send();
+};
+
+/**
+ * Parses response from Train Enquiry and displays the required running
+ * status
+ *
+ * @param data Object Response recieved from the server. Null if the
+ *      request failed.
+ */
+function parseStatus(data) {
+    if(data) {
+        $('#result').html(data);
+    } else {
+        $('#running-status').hide(500);
+        $('#result').html('<b>Some error occured while processing the request.</b>');
+    }
+};
+
+/**
+ * Click handler for the submit button. It validates the params entered
+ * and calls fetchData with those.
+ *
+ */
+function RunningStatus() {
+    var inputs = ['tr','st','dt'], data = {};
+    $.each(inputs, function (k, v) { data[v] = $(':input[name='+v+']').val(); });
+    var params = $.param(data);
+    fetchData(parseStatus, params);
+};
+
